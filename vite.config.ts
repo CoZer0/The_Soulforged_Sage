f256a747ecb226cb6d75a01eb9d140408ec5d44f
aashwin504 <aashwin504@gmail.com>
2025-12-08 06:51:57 +0000

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
+'''
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -18,6 +21,15 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
-      }
+      },
+      css: {
+        postcss: {
+          plugins: [
+            tailwindcss,
+            autoprefixer,
+          ],
+        },
+      },
     };
 });
+'''
